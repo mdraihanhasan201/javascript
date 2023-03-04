8cc24197a45751cdae1bba868cf0acf3abe4113c
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-03-04 20:35:13 +0600

    Add files via upload

---
+++ first.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+var fname=prompt("enter your first name");
+var lname=prompt("enter your last name");
+console.log("your full name",fname,lname)
+console.log(" you are welcome now")
