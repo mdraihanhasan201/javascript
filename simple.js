d27cbead5a966fbd21a0178debf0f87b92ee9a4a
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-03-12 23:57:32 +0600

    Add files via upload

---
+++ simple.js
@@ -0,0 +1,15 @@
+const arrayLike = {
+    length: 3,
+    unrelated: "foo",
+    2: 4,
+  };
+  console.log(Array.prototype.shift.call(arrayLike));
+  // undefined, because it is an empty slot
+  console.log(arrayLike);
+  // { '1': 4, length: 2, unrelated: 'foo' }
+  
+  const plainObj = {};
+  // There's no length property, so the length is 0
+  Array.prototype.shift.call(plainObj);
+  console.log(plainObj);
+  // { length: 0 }
