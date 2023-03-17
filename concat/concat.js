59f3fc4982ad748934b6854f1b6f9ed6d7cabc70
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-03-17 18:09:42 +0600

    Add files via upload
    how to use concat function;

---
+++ concat/concat.js
@@ -0,0 +1,12 @@
+const array1=["a","b","c","d"]
+const array2 =["e","f","g"]
+const array4=["h"]
+
+const array3=array1.concat(array2,array4);
+console.log(array3);
+
+
+
+const number= ["2","4","6"]
+var item = number.concat(1,[2,3]);
+console.log(item);
