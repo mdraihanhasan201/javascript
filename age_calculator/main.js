0bf04ecefd8917699dc996b8eb16155d1a2d35a3
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-08-14 22:23:52 +0600

    Add files via upload
    3 times edit

--- age_calculator/main.js
+++ age_calculator/main.js
@@ -5,10 +5,10 @@ let reset = document.getElementById("reset")
 clickme.onclick = function(){
     var birthday = document.getElementById("get-age").value;
     if (birthday == ""){
-       alert("enter your birthday")   ;
+        document.getElementById("fillup").innerHTML="enter your birthday"  ;
     } else{
         let age = 2023-birthday;
-        document.getElementById("get-age").value=age;
+        document.getElementById("get-age").value="your current age is "+age;
 
     }
 
