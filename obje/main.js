b41c69dd8a30895afd15bec0236dc8e3212c38d8
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-09-19 20:11:00 +0600

    Add files via upload

---
+++ obje/main.js
@@ -0,0 +1,22 @@
+let student={first:'rayhan',
+last:"islam",
+age:23,
+info:function(){
+    return("my name is "+ this.first+"  and i am  "+this.age+"years old");
+}
+
+
+};
+// console.log(student.first);
+// console.log(student.last);
+
+student.age++
+console.log(student.info());
+
+
+
+
+
+// let student ={
+
+// }
